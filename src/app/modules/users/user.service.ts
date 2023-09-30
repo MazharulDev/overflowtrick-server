ba5f2305d6af7f4578c6a9bfbb588d9d33944ce1
Md Mazharul Islam <mdmazharulislam.dev@gmail.com>
2023-09-30 17:31:21 +0600

    user create throw error taken username

--- src/app/modules/users/user.service.ts
+++ src/app/modules/users/user.service.ts
@@ -8,8 +8,18 @@ import { User } from "./user.model";
 import { IPost } from "../post/post.interface";
 import { Post } from "../post/post.model";
 import { Comment } from "../comment/comment.model";
+import ApiError from "../../../errors/ApiError";
+import httpStatus from "http-status";
 
 const createUser = async (payload: IUser): Promise<IUser> => {
+  const userExist = await User.find({ username: payload?.username });
+  const exist = userExist[0]?.username;
+  if (payload.username === exist) {
+    throw new ApiError(
+      httpStatus.BAD_REQUEST,
+      "username is taken, Enter another username"
+    );
+  }
   const result = await User.create(payload);
   return result;
 };
